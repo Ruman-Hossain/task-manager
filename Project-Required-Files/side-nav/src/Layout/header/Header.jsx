80553cbb2338494e6ec78fe1515d022fad4235d7
SM Feroj <sm.feroj@gmail.com>
2023-03-11 22:28:01 +0600

    sidenav creation

---
+++ Project-Required-Files/side-nav/src/Layout/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css'
+import Sidebar from '../../components/Sidebar'
+
+const Header = () => {
+  return (
+    <div>
+     <Sidebar/>
+    </div>
+  )
+}
+
+export default Header
